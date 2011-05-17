516c0f1e8fad100141aff67169310d1e1fd4a618
Christian Hoffmeister <choffmeister.github@googlemail.com>
2011-05-17 08:27:41 +0200

    Created bundle skeleton

---
+++ Tests/ThekwastiWikiBundleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Thekwasti\WikiBundle\Tests;
+
+use Thekwasti\WikiBundle\ThekwastiWikiBundle;
+
+class ThekwastiWikiBundleTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $bundle = new ThekwastiWikiBundle();
+    }
+}
---
+++ ThekwastiWikiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Thekwasti\WikiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ThekwastiWikiBundle extends Bundle
+{
+}
